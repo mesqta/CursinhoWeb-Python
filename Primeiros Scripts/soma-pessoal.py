6cd454d2fd80da024040905ca1b93dde930b85f1
mesqta <meskita@proton.me>
2024-01-06 21:10:49 -0300

    nova soma

---
+++ Primeiros Scripts/soma-pessoal.py
@@ -0,0 +1,8 @@
+nome = input('Qual seu nome?: ')
+idade = input('Qual sua idade?: ')
+peso = input('Qual seu peso?: ')
+dia = input('Qual foi o dia que voce nasceu?: ')
+mes = input('Qual foi o mes que voce nasceu?: ')
+ano = input('Qual foi o ano que voce nasceu?: ')
+
+print('Olá', nome, 'Seja bem vindo(a)', 'sua idade é:', idade, 'Correto?', 'seu peso é:', peso, 'Correto?', 'Voce nasceu no dia', dia, 'Do mes', mes, 'do ano', ano, 'Certo?')
