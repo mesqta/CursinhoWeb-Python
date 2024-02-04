bc701778639202a64a89a4b3725157806978d11d
Mesqta <meskita@proton.me>
2024-02-04 00:06:45 -0300

    commit

--- 7- Condições Aninhadas/01painel.py
+++ 7- Condições Aninhadas/01painel.py
@@ -1,40 +1,62 @@
 import math
+from time import sleep
+
 nome = input('Qual seu nome?: ').strip().capitalize()
 idade = input(f'Otimo {nome}, agora qual é a sua idade?: ')
 confirmar = input(f'Seu nome é "{nome}" e a sua idade {idade}? [sim/nao]: ').lower()
 
 if confirmar == 'sim':
-    perguntar = input(f'{nome} Voce gostaria de ver meu painel matematico? [sim/nao]: ').lower()
-    if perguntar == 'sim':
-        painel = input('[1] | SOMA SIMPLES\n[2] | RAIZ QUADRADA\n[OPÇÕES]: ')
-        if painel == '1':
-            print(f'{nome} Voce selecionou soma simples!')
-            n1 = int(input('Primeiro numero: '))
-            n2 = int(input('Segundo numero: '))
-            soma = n1 + n2
-            print(f'O resultado entre {n1} e {n2} é: {soma}')
-            feed = input('A soma esta correta? [sim/nao]: ').lower()
-            if feed == 'sim':
-                print('Muito bom, obrigado por confiar em mim.')
-            elif feed == 'nao':
-                print('Irei melhorar meu codigo :C')
-        else:
-            print('Opção invalida!')
-        if painel == '2':
-            print(f'{nome} Voce selecionou a Raiz Quadrada!')
-            n1 = int(input('Digite um numero: '))
-            raiz = math.sqrt(n1)
-            print(f'A Raiz quadrada de {n1} é:  {raiz :.2f}')
-            print(f'Para cima: {math.ceil(raiz) :.2f}')
-            print(f'Para baixo: {math.floor(raiz) :.2f}')
-            feed = input('Está correto? [sim/nao]: ').lower()
-            if feed == 'sim':
-                print('Que bom que meu comando está certo! :D')
-            elif feed == 'nao':
-                print('Então vou corrigi-lo em breve :v')
-        else:
-            print('Opção invalida!')
-    elif perguntar == 'nao':
-        print('Tudo bem, até mais!')
+    pergunta = input(f'Seu nome é {nome} e voce tem {idade} anos, deseja continuar? [sim/nao]: ').lower()
+elif confirmar == 'nao':
+    print('Você pode voltar quando quiser.')
 else:
-    print('Você digitou errado tente novamente!')
+    print('Resposta inválida.')
+    
+if pergunta == 'sim':
+    painel = input('Otimo, voce gostaria de ver meu painel com 2 funções mátematicas? [sim/nao]: ').lower()
+elif pergunta == 'nao':
+    print('Você pode voltar quando quiser.')
+else:
+    print('Resposta inválida.')
+    
+if painel == 'sim':
+    mostrar = input('[1] | SOMA\n[2] | RAIZ QUADRADA\n[OPÇÃO]: ')
+    if painel == 'nao':
+        print('Você pode voltar quando quiser.')
+else:
+    print('Resposta inválida.')
+    
+#  PAINEL COM FUNÇÕES MATEMÁTICAS
+if mostrar == '1':
+    print('Você escolheu a soma!')
+    n1 = int(input('Primeiro numero: '))
+    n2 = int(input('Segundo numero: '))
+    soma = n1 + n2
+    print('Calculando...')
+    sleep(2)
+    print(f'O resultado entre {n1} e {n2} é: {soma}')
+    resultado = input('Resultado está certo? [sim/nao]: ').lower()
+    if resultado == 'sim':
+        print('Excelente!')
+    elif resultado == 'nao':
+        print('Desculpe, irei corrigir brevemente.')
+else:
+    print('Resposta inválida.')
+#    PAINEL DA RAIZ QUADRADA
+    if mostrar == '2':
+        print('Você escolheu a Raiz quadrada!')
+        num = int(input('Digite um numero: '))
+        raiz = math.sqrt(num)
+        print('Calculando...')
+        sleep(2)
+        print(f'A raiz quadrada de {num} é: {raiz :.2f}')
+        print(f'A raiz quadrada de {num} para cima é: {math.ceil(raiz ) :.2f}')
+        print(f'A raiz quadrada de {num} para baixo é: {math.floor(raiz) :.2f}')
+        resultado = input('Resultado está certo? [sim/nao]: ').lower()
+        if resultado == 'sim':
+            print('Excelente!')
+        elif resultado == 'nao':
+            print('Desculpe, irei corrigir brevemente.')
+    else:
+        print('Resposta inválida.')
+exit()
